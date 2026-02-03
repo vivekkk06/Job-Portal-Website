93c9de3bbe35b2c802cff1ef709ecd707e499e85
VIVEK BADGUJAR <vivek303323@gmail.com>
2026-02-03 16:27:43 +0530

    Final production email config fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,12 +137,16 @@ CORS_ALLOW_CREDENTIALS = True
 # ==============================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 DEFAULT_FROM_EMAIL = f"JobDhundho <{EMAIL_HOST_USER}>"
 
