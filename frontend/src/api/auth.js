7f41602480835d695a35b56d1fa2982caba2c4e3
VIVEK BADGUJAR <vivek303323@gmail.com>
2026-02-03 10:26:47 +0530

    Fix missing api prefix

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,21 +1,10 @@
 import api from "./axios";
 
-// LOGIN
 export const login = (data) =>
   api.post("/api/accounts/login/", data);
 
-// START REGISTER
 export const register = (data) =>
   api.post("/api/accounts/start-register/", data);
 
-// VERIFY EMAIL (if used)
-export const verifyEmail = (data) =>
-  api.post("/api/accounts/verify-email/", data);
-
-// COMPLETE REGISTER (if used)
-export const completeRegister = (data) =>
-  api.post("/api/accounts/complete-register/", data);
-
-// GET CURRENT USER
 export const me = () =>
   api.get("/api/accounts/me/");
--- frontend/src/api/jobs.js
+++ frontend/src/api/jobs.js
@@ -1,13 +1,10 @@
 import api from "./axios";
 
-// Get all jobs
 export const getJobs = () =>
   api.get("/api/jobs/");
 
-// Get single job
 export const getJob = (id) =>
   api.get(`/api/jobs/${id}/`);
 
-// Apply for job
 export const applyJob = (jobId) =>
   api.post("/api/applications/", { job: jobId });
