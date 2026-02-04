04bafda6e32dc890939299ee9b33e8cd9b707895
VIVEK BADGUJAR <vivek303323@gmail.com>
2026-02-04 10:00:06 +0530

    Fix application serializer

--- backend/apps/applications/serializers.py
+++ backend/apps/applications/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
 from .models import Application
 
+
 class ApplicationSerializer(serializers.ModelSerializer):
+
     resume_url = serializers.SerializerMethodField()
     job_title = serializers.CharField(source="job.title", read_only=True)
 
     class Meta:
         model = Application
         fields = "__all__"
+
+        # 🔥 VERY IMPORTANT
+        read_only_fields = [
+            "applicant",
+            "job",
+            "status",
+            "applied_at",
+        ]
 
     def get_resume_url(self, obj):
         request = self.context.get("request")
