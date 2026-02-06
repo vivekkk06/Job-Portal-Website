62a48fe2e19a70041988880aeef715efe25b2218
VIVEK BADGUJAR <vivek303323@gmail.com>
2026-02-06 18:39:02 +0530

    fix emal sending issues

--- backend/settings.py
+++ backend/settings.py
@@ -1,12 +1,17 @@
 from decouple import config
 
+# ==============================
+# SENDGRID EMAIL CONFIG
+# ==============================
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "apikey"  
+EMAIL_HOST_USER = "apikey"
 EMAIL_HOST_PASSWORD = config("SENDGRID_API_KEY")
+
 
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
