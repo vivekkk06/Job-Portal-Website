7eb2327620a9d2a306cc7e4f4142697b8fe07376
VIVEK BADGUJAR <vivek303323@gmail.com>
2026-02-06 19:02:21 +0530

    Fix email issues

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,15 +148,12 @@ CORS_ALLOW_CREDENTIALS = True
 # SENDGRID EMAIL
 # ==============================
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = config("SENDGRID_API_KEY")
+SENDGRID_API_KEY = config("SENDGRID_API_KEY")
 
-DEFAULT_FROM_EMAIL = "kenichi13112006@gmail.com"
-
+DEFAULT_FROM_EMAIL = config(
+    "DEFAULT_FROM_EMAIL",
+    default="kenichi13112006@gmail.com"
+)
 
 # ==============================
 # CLOUDINARY (🔥 CORRECT WAY)
